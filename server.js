6fdc966cec777d96b715685a02e866e22cc7f471
Annexe-ITAI <annexe.itai@gmail.com>
2026-07-04 16:24:03 +0800

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,23 @@ const {
 // SCOPES
 // =============================
 const SCOPES = [
-  "publicData"
+  "publicData",
+  "esi-location.read_location.v1",
+  "esi-mail.read_mail.v1",
+  "esi-wallet.read_character_wallet.v1",
+  "esi-clones.read_clones.v1",
+  "esi-characters.read_contacts.v1",
+  "esi-assets.read_assets.v1",
+  "esi-characters.read_chat_channels.v1",
+  "esi-characters.read_standings.v1",
+  "esi-industry.read_character_jobs.v1",
+  "esi-markets.read_character_orders.v1",
+  "esi-characters.read_corporation_roles.v1",
+  "esi-contracts.read_character_contracts.v1",
+  "esi-clones.read_implants.v1",
+  "esi-corporations.read_contacts.v1",
+  "esi-corporations.read_standings.v1",
+  "esi-characters.read_titles.v1"
 ].join(" ");
 
 // =============================
