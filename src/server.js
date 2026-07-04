32e8e9b196857102eb5e311877133ee86dc321ac
Annexe-ITAI <annexe.itai@gmail.com>
2026-07-04 20:50:26 +0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import authRoutes from "./routes/auth.js";
 import callbackRoutes from "./routes/callback.js";
 import apiRoutes from "./routes/api.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use("/auth/eve", authRoutes);
 app.use("/auth/eve/callback", callbackRoutes);
 app.use("/api", apiRoutes);
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("EVE Recruiter API is running");
