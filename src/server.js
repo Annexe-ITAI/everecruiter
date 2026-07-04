f429edc6fa8739b3beb85a5fe0fc1100dc0e5bb9
Annexe-ITAI <annexe.itai@gmail.com>
2026-07-05 00:26:22 +0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import authRoutes from "./routes/auth.js";
 import callbackRoutes from "./routes/callback.js";
 import apiRoutes from "./routes/api.js";
@@ -7,11 +8,20 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+  origin: "https://recruit.inextremis.co",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+
+app.options("*", cors());
 
 app.use("/auth/eve", authRoutes);
 app.use("/auth/eve/callback", callbackRoutes);
 app.use("/api", apiRoutes);
-app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("EVE Recruiter API is running");
